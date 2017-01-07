a24ecaacaefcaf70cf9557e5c212401a6203a8bc
Thomas <thomasdes533@gmail.com>
2017-01-07 11:39:05 -0800

    Refactoring Added multiply utterance

--- NumberFunctions/NumberFunctions/Function.cs
+++ NumberFunctions/NumberFunctions/Function.cs
@@ -2,6 +2,7 @@ using Amazon.Lambda.Core;
 using Slight.Alexa.Framework.Models.Requests;
 using Slight.Alexa.Framework.Models.Responses;
 using System;
+using System.Diagnostics;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializerAttribute(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
@@ -13,49 +14,41 @@ namespace NumberFunctions
         public SkillResponse FunctionHandler(SkillRequest input, ILambdaContext context)
         {
             IOutputSpeech innerResponse = null;
-            var log = context.Logger;
+            var result = 0;
 
             if (input.GetRequestType() == typeof(Slight.Alexa.Framework.Models.Requests.RequestTypes.ILaunchRequest))
             {
-                // default launch request, let's just let them know what you can do
-                log.LogLine($"Default LaunchRequest made");
-
                 innerResponse = new PlainTextOutputSpeech();
-                ((PlainTextOutputSpeech) innerResponse).Text = "Welcome to number functions.  You can ask us to add numbers!";
+                ((PlainTextOutputSpeech)innerResponse).Text = "Welcome to number functions.  You can ask us to add numbers!";
             }
             else if (input.GetRequestType() == typeof(Slight.Alexa.Framework.Models.Requests.RequestTypes.IIntentRequest))
             {
+                Debug.WriteLine(input.Request.Intent.Name);
+
                 var firstNumber = Convert.ToDouble(input.Request.Intent.Slots["firstnum"].Value);
                 var secondNumber = Convert.ToDouble(input.Request.Intent.Slots["secondnum"].Value);
-                if (input.Request.Intent.Name == "AddIntent")
+                switch (input.Request.Intent.Name)
                 {
-                    double result = firstNumber + secondNumber;
-
-                    innerResponse = new PlainTextOutputSpeech();
-                    ((PlainTextOutputSpeech) innerResponse).Text = $"The result is {result}.";
-                }
-                else if (input.Request.Intent.Name == "SubtractIntent")
-                {
-                    double result = firstNumber - secondNumber;
-
-                    innerResponse = new PlainTextOutputSpeech();
-                    ((PlainTextOutputSpeech) innerResponse).Text = $"The result is {result}.";
-                }
-                else if (input.Request.Intent.Name == "MultiplyIntent")
-                {
-                    double result = firstNumber * secondNumber;
-
-                    innerResponse = new PlainTextOutputSpeech();
-                    ((PlainTextOutputSpeech)innerResponse).Text = $"The result is {result}.";
-                }
-                else if (input.Request.Intent.Name == "DivideIntent")
-                {
-                    double result = firstNumber / secondNumber;
-
-                    innerResponse = new PlainTextOutputSpeech();
-                    ((PlainTextOutputSpeech)innerResponse).Text = $"The result is {result}.";
+                    case "AddIntent":
+                        result = (int) (firstNumber + secondNumber);
+                        break;
+                    case "SubtractIntent":
+                        result = (int) (firstNumber - secondNumber);
+                        break;
+                    case "MultiplyIntent":
+                        result = (int) (firstNumber * secondNumber);
+                        break;
+                    case "DivideIntent":
+                        result = (int) (firstNumber / secondNumber);
+                        break;
+                    default:
+                        result = 42;
+                        break;
                 }
             }
+
+            innerResponse = new PlainTextOutputSpeech();
+            ((PlainTextOutputSpeech)innerResponse).Text = $"The result is {result}.";
 
             var response = new Response
             {
